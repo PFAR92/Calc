2ac55f5544f76ba42ffaedc45dd3733e1d05f339
Paulo Felipe <paulofelipetj@hotmail.com>
2022-10-03 13:47:23 -0300

    Main (#5)
    * 03/10/22
    
    * 03/10/22
    
    * 03/10/22

---
+++ src/Controlador.java
@@ -0,0 +1,7 @@
+public interface Controlador {
+    public abstract boolean verificaInputs(String valor);
+    public abstract float soma();
+    public abstract float subtracao();
+    public abstract float multiplicacao();
+    public abstract float divisao();
+}
